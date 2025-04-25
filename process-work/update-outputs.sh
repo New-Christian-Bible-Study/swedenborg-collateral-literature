a908466a9485fac3cdaddcb24adb77c689ca9ff8
mh3riley <maynard@angelharbor.org>
2025-04-25 17:57:34 -0500

    cleanup workspace

--- process-work/update-outputs.sh
+++ process-work/update-outputs.sh
@@ -6,9 +6,10 @@
 # 	if adoc last modified, create a new one
 
 sourcedir='../text'
-pdfdest='/ssd1/Concordance/publish/pdfs'
-epubdest='/ssd1/Concordance/publish/epubs'
-mobidest='/ssd1/Concordance/publish/mobis'
+destbase='../../publish'
+pdfdest=$destbase/pdfs
+epubdest=$destbase/epubs
+mobidest=$destbase/mobis
 
 [ -f /tmp/adoc.log ] && rm /tmp/adoc.log
 
