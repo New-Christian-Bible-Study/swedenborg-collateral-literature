2bc95c203650bca055703033138efd92e6862d64
mh3riley <maynard@angelharbor.org>
2026-02-28 10:36:47 -0600

    all chapters written; for perusal and processing [tests]

--- BSN-Dole/scripts/aggregate-chapter.sh
+++ BSN-Dole/scripts/aggregate-chapter.sh
@@ -6,17 +6,17 @@
 # script resides in BSN_Dole/scripts
 
 source_directory='disaggregants'
+target_directory='chapters'
 
 front_matter="Edited and Printed by NCBS + \nfrom best available representations of original manuscript.\n\n[.text-right]\nLast Revised {docdate}\n"
 
 
-if [ $1 -ge 2 ]; then chapter_number=$1
+if [ $1 -ge 1 ]; then chapter_number=$1
 	else echo "script requires numeric chapter number on command line"; exit
 fi
 
 chapter_number=$1
 title_number=$((chapter_number-1))
-output_file="Chapter_$chapter_number.adoc"
 chapter_directory=$(ls -1d $source_directory/$chapter_number:*)
 chapter_title=${chapter_directory#$source_directory/}
 # sample: Title: 53:THE DIVISION OF THE LAND — Joshua 18:1-10
@@ -24,6 +24,7 @@ bible_ref=${chapter_title##*$'\u2014' }
 chapter_name=${chapter_title% $'\u2014'*}
 chapter_name=${chapter_name#*:}
 title=$(title_case $chapter_name)
+output_file="$target_directory/$title_number:$chapter_name.adoc"
 
 echo "Reading: $chapter_directory into: ./$output_file"
 #echo "Title: $chapter_title"
